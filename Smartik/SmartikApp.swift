6cea55a6e2a370faade759d64c545fb918f372cc
misaroshm <102255051+misaroshm@users.noreply.github.com>
2025-05-12 11:52:23 +0300

    Initial Commit

---
+++ Smartik/SmartikApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartikApp.swift
+//  Smartik
+//
+//  Created by Mariia Misarosh on 12.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct SmartikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
